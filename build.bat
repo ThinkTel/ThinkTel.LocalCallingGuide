ad188ea24821533266d72ed11a3bd29f772acfdd
Paul Vaillant <paulv@thinktel.ca>
2015-01-22 13:10:27 -0800

    Fix build.bat

--- build.bat
+++ build.bat
@@ -23,19 +23,19 @@ rem )
 
 REM Package restore
 %nuget% install .nuget\packages.config -OutputDirectory %cd%\packages -NonInteractive
-%nuget% install ThinkTel.uControl.Api.Tests\packages.config -OutputDirectory %cd%\packages -NonInteractive
+%nuget% install ThinkTel.LocalCallingGuide.Tests\packages.config -OutputDirectory %cd%\packages -NonInteractive
 
 REM Build
 mkdir build
-"%MsBuildExe%" ThinkTel.uControl.Api.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=build\msbuild.log;Verbosity=Normal /nr:false
+"%MsBuildExe%" ThinkTel.LocalCallingGuide.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=build\msbuild.log;Verbosity=Normal /nr:false
 if not "%errorlevel%"=="0" goto failure
 
 REM Unit tests
-%XUnit192Path%\xunit.console.clr4.exe ThinkTel.uControl.Api.Tests\bin\%config%\ThinkTel.uControl.Api.Tests.dll
+%XUnit192Path%\xunit.console.clr4.exe ThinkTel.LocalCallingGuide.Tests\bin\%config%\ThinkTel.LocalCallingGuide.Tests.dll
 if not "%errorlevel%"=="0" goto failure
 
 REM Package
-cmd /c %nuget% pack "ThinkTel.uControl.Api\ThinkTel.uControl.Api.nuspec" -symbols -o build -p Configuration=%config% %version%
+cmd /c %nuget% pack "ThinkTel.LocalCallingGuide\ThinkTel.LocalCallingGuide.nuspec" -symbols -o build -p Configuration=%config% %version%
 if not "%errorlevel%"=="0" goto failure
 
 :success
